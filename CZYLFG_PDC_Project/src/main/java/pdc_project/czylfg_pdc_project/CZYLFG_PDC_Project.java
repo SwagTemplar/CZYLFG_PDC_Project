2ebde8d47b7caa8fd33356f17891d8e24dd6b2ca
SwagTemplar <38060224+SwagTemplar@users.noreply.github.com>
2022-08-08 17:22:52 +1200

    Created Maven Java Project
    Initial launch of the PDC project.

---
+++ CZYLFG_PDC_Project/src/main/java/pdc_project/czylfg_pdc_project/CZYLFG_PDC_Project.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package pdc_project.czylfg_pdc_project;
+
+/**
+ *
+ * @author Kisoon Park 18008173
+ */
+public class CZYLFG_PDC_Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

df9a3c02959a81eeb411d480607c8261812f8d70
Conzeyy <connorstwrt@gmail.com>
2022-08-31 11:17:01 +1200

    Order creation

---
+++ CZYLFG_PDC_Project/src/main/java/pdc_project/czylfg_pdc_project/Order.java
@@ -0,0 +1,80 @@
+package pdc_project.czylfg_pdc_project;
+
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.Scanner;
+
+/*
+ * @author "Connor Stewart 17982915"
+ */
+public class Order {
+
+    public void Order() {
+        String itemName = null;
+
+        String itemQuant = null;
+
+        try {
+
+            Scanner sc = new Scanner(System.in);
+
+            boolean isValid = false;
+            while (!isValid) {
+                String userExit = sc.nextLine();
+                if (userExit.trim().equalsIgnoreCase("x")) {
+                    System.out.println("Back to menu");
+                    System.exit(0);
+                }
+                BufferedReader readIn = new BufferedReader(new InputStreamReader(System.in));
+
+                System.out.println("Enter the fruit you want to order");
+                itemName = readIn.readLine();
+
+                System.out.println("Enter the quantity you want to order");
+                itemQuant = readIn.readLine();
+
+                System.out.println("You are ordering: " + itemQuant + " " + itemName);
+
+                PrintStream printOut = new PrintStream(new FileOutputStream("./dbresources/Send_inv.txt", true), true);
+
+                printOut.println(itemName);
+                printOut.println(itemQuant);
+
+                printOut.close();
+                readIn.close();
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error reading from file ");
+
+        }
+        //// kisoon this is just a tester method so it'll print the line and what number the line is, 
+        //// it also sets it so when printing the lines they start at 0 but it wont push them in the doc to different lines.
+        try {
+            LineNumberReader readingInv = new LineNumberReader(new FileReader("./dbresources/Send_inv.txt"));
+
+            readingInv.setLineNumber(0);
+
+            String line = null;
+            while ((line = readingInv.readLine()) != null) {
+                System.out.println("Line " + readingInv.getLineNumber() + ": " + line);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error reading from file ");
+
+        }
+    }
+}
+
+//    public static HashMap<String, Integer> readOrders()
+//    {
+//        
+//    }
+
--- CZYLFG_PDC_Project/src/main/java/pdc_project/czylfg_pdc_project/TestMain.java
+++ CZYLFG_PDC_Project/src/main/java/pdc_project/czylfg_pdc_project/TestMain.java
@@ -9,7 +9,7 @@ import java.util.*;
  */
 public class TestMain {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Fruit apple1 = new GalaApple(1000);
         System.out.println("Name: " + apple1.fruitName + " ID: " + apple1.fruitID);
         System.out.println("Type of Apple: " + apple1.getFruitName());
@@ -26,26 +26,34 @@ public class TestMain {
         System.out.println("Name: " + b.fruitName + " ID: " + b.fruitID);
 
 //        Inventory obj = new Inventory();
+          Order orderObj = new Order();
         //// This is just reading user input that is put into the console and saves it into the .txt file I added into the folder dbresources
         //// idk what happened, it was working then it didnt and started to create .txt files that were 4gb+ broke it so now it doesnt save content to .txt files
-        try {
-            BufferedReader readIn = new BufferedReader(new InputStreamReader(System.in));
 
-            String itemName = readIn.readLine();
-            String itemQuant = readIn.readLine();
+        // Orderobj testing here
 
-            System.out.println("You are ordering: " + itemQuant + " " + itemName);
 
-            PrintStream printOut = new PrintStream(new FileOutputStream("./dbresources/Send_inv.txt",true),true);
-
-            printOut.close();
-            readIn.close();
-
-        } catch (IOException e) {
-            System.out.println("Error reading from file ");
-        }
-        //// kisoon this is just a tester method so it'll print the line and what number the line is, 
-        //// it also sets it so when printing the lines they start at 0 but it wont push them in the doc to different lines.
+//        try {
+//            BufferedReader readIn = new BufferedReader(new InputStreamReader(System.in));
+//
+//            String itemName = readIn.readLine();
+//            String itemQuant = readIn.readLine();
+//
+//            System.out.println("You are ordering: " + itemQuant + " " + itemName);
+//
+//            PrintStream printOut = new PrintStream(new FileOutputStream("./dbresources/Send_inv.txt", true), true);
+//            
+//            printOut.println(itemName);
+//            printOut.println(itemQuant);
+//            
+//            printOut.close();
+//            readIn.close();
+//        } catch (IOException e) {
+//            System.out.println("Error reading from file ");
+//
+//        }
+//        //// kisoon this is just a tester method so it'll print the line and what number the line is, 
+//        //// it also sets it so when printing the lines they start at 0 but it wont push them in the doc to different lines.
 //        try {
 //            LineNumberReader readingInv = new LineNumberReader(new FileReader("./dbresources/Send_inv.txt"));
 //
